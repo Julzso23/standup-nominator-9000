2be21cbee7331df15022e11f91a9e8de3e271c76
Julian <Julzso23@users.noreply.github.com>
2021-01-23 13:32:23 +0000

    Replaced the old wheel with the new one

--- src/components/Wheel.vue
+++ src/components/Wheel.vue
@@ -1,47 +1,55 @@
 <template>
-  <div class="overlay" :class="{hidden: !visible}" v-if="prizes.length !== 0">
-    <FortuneWheel
-      ref="wheel"
-      class="wheel"
-      :canvas="canvasOptions"
-      :prizes="prizes"
-      :verify="false"
-      :useWeight="true"
-      :duration="duration"
-      @rotateEnd="onRotateEnd"
-    />
+  <div class="overlay">
+    <div class="wheel-container">
+      <svg preserveAspectRatio="xMidYMid meet" :viewBox="`0 0 ${size} ${size}`" class="wheel" :style="wheelStyle" @transitionend="onRotateEnd">
+        <defs>
+          <clipPath id="clippath">
+            <path :d="clipPath"></path>
+          </clipPath>
+        </defs>
+        <circle :cx="halfSize" :cy="halfSize" :r="size"></circle>
+        <path v-for="(path, index) in svgPaths" :key="'segment' + index" :d="path.d" :fill="path.fill"></path>
+        <text v-for="(path, index) in svgPaths" :key="'label' + index" :x="5" :y="halfSize" dominant-baseline="central" :clip-path="`url(#clippath)`" :transform="`rotate(${(index + 0.5) * (360 / availablePeople.length) - 180}, ${halfSize}, ${halfSize})`">{{ path.name }}</text>
+      </svg>
+
+      <div class="arrow"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import FortuneWheel from 'vue-fortune-wheel'
-import 'vue-fortune-wheel/lib/vue-fortune-wheel.css'
-
 export default {
-  name: 'wheel',
-  components: {
-    FortuneWheel
-  },
+  name: 'wheel-2',
   props: {
     people: Array,
-    visible: Boolean,
     duration: Number
   },
   data: () => ({
-    canvasOptions: {
-      borderWidth: 6,
-      borderColor: '#584b43',
-      textDirection: 'vertical',
-      fontSize: 20,
-      textLength: 10,
-      textRadius: 230,
-      btnText: '',
-      btnWidth: 100
-    }
+    winner: -1,
+    size: 400
   }),
+  mounted () {
+    setTimeout(() => this.winner = Math.floor(Math.random() * this.availablePeople.length), 100)
+  },
   computed: {
-    prizes () {
-      const results = []
+    availablePeople () {
+      return this.people.filter(person => person.available)
+    },
+    wheelStyle () {
+      if (this.winner === -1) {
+        return `
+          transition-duration: 0ms;
+          transform: rotate(0deg);`
+      }
+      return `
+        transition-duration: ${this.duration}ms;
+        transform: rotate(${-90 - (this.winner + 0.5) * (360 / this.availablePeople.length) + (360 * (this.duration / 1000))}deg);`
+    },
+    halfSize () {
+      return this.size / 2
+    },
+    svgPaths () {
+      const paths = []
 
       const colours = [
         '#ff5555',
@@ -52,43 +60,45 @@ export default {
         '#55ffff'
       ]
 
-      let id = 0
-      for (const person of this.people) {
-        if (!person.available) continue
-        id++
-        results.push({
-          id: id, //* The unique id of each prize, an integer greater than 0
-          name: person.name, // Prize name, display value when type is canvas (this parameter is not needed when type is image)
-          value: person.name, //* Prize value, return value after spinning
-          bgColor: colours[(id - 1) % colours.length], // Background color (no need for this parameter when type is image)
-          color: '#111', // Font color (this parameter is not required when type is image)
-          weight: 1 // Weight, if useWeight is true, the probability is calculated by weight (weight must be an integer), so probability is invalid
+      for (const i in this.availablePeople) {
+        const largeArc = this.availablePeople.length === 1 ? 1 : 0
+        let angleSize = Math.PI * 2 / this.availablePeople.length
+        if (this.availablePeople.length === 1) angleSize = Math.PI * 2 - 0.0001
+        const anglePosition = angleSize * i
+        paths.push({
+          name: this.availablePeople[i].name,
+          d: `
+            M${this.halfSize},${this.halfSize}
+            L${this.halfSize + this.halfSize * Math.cos(anglePosition)},${this.halfSize + this.halfSize * Math.sin(anglePosition)}
+            A${this.halfSize},${this.halfSize} 0 ${largeArc},1 ${this.halfSize + this.halfSize * Math.cos(anglePosition + angleSize)},${this.halfSize + this.halfSize * Math.sin(anglePosition + angleSize)} z`,
+          fill: colours[i % colours.length]
         })
       }
 
-      return results
+      return paths
+    },
+    clipPath () {
+      if (this.availablePeople.length === 0) return ''
+
+      const largeArc = this.availablePeople.length === 1 ? 1 : 0
+      let angleSize = Math.PI * 2 / this.availablePeople.length
+      if (this.availablePeople.length === 1) angleSize = Math.PI * 2 - 0.0001
+      const anglePosition = Math.PI - (angleSize / 2)
+      return `
+        M${this.halfSize},${this.halfSize}
+        L${this.halfSize + this.halfSize * Math.cos(anglePosition)},${this.halfSize + this.halfSize * Math.sin(anglePosition)}
+        A${this.halfSize},${this.halfSize} 0 ${largeArc},1 ${this.halfSize + this.halfSize * Math.cos(anglePosition + angleSize)},${this.halfSize + this.halfSize * Math.sin(anglePosition + angleSize)} z`
     }
   },
   methods: {
-    onRotateEnd (prize) {
-      this.$emit('nominate', prize.value)
-    },
-    spin () {
-      this.$nextTick(() => setTimeout(() => this.$refs.wheel.handleClick(), 100)) // Stupid hack to stop it instantly spinning to the end (I hate this)
-    }
-  },
-  watch: {
-    prizes () {
-      this.$nextTick(() => {
-        this.$refs.wheel.drawCanvas()
-        this.$refs.wheel.rotateEndDeg = 0
-      })
+    onRotateEnd () {
+      this.$emit('onRotateEnd', this.availablePeople[this.winner].name)
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .overlay {
     position: fixed;
     left: 0;
@@ -98,20 +108,31 @@ export default {
     background: rgba(0, 0, 0, 0.5);
     display: flex;
     align-items: center;
-  }
+    justify-content: center;
 
-  .wheel {
-    width: 90%;
-    left: 5%;
-  }
+    .wheel-container {
+      overflow: hidden;
+      position: relative;
+      width: 90%;
 
-  .hidden {
-    display: none;
-  }
-</style>
+      .wheel {
+        border-radius: 50%;
+        overflow: hidden;
+        border: solid 0.5rem #333;
+        width: 100%;
+      }
 
-<style>
-  .fw-btn__btn {
-    cursor: pointer !important;
+      .arrow {
+        width: 0;
+        height: 0;
+        border-left: 1rem solid transparent;
+        border-right: 1rem solid transparent;
+        border-top: 2rem solid #333;
+        position: absolute;
+        top: 0.4rem;
+        left: 50%;
+        transform: translate(-50%, 0);
+      }
+    }
   }
 </style>
