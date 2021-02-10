826b978bfea5bfddd1cfb2d86f3c32aad09e8aee
Julian <Julzso23@users.noreply.github.com>
2021-02-10 12:34:23 +0000

    Added electron builder config Allows changing the version without duplicating windows installed program entries

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'website.julians.standup-nominator-9000',
+        productName: 'Standup Nominator 9000',
+        nsis: {
+          createDesktopShortcut: false,
+          uninstallDisplayName: '${productName}'
+        }
+      }
+    }
+  }
+}
